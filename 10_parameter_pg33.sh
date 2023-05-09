6ce675344c7d40936dc67a1cc4e24774c4f08926
cwnam14 <cwnam14@gmail.com>
2023-05-09 17:39:57 +0900

    Delete 10_parameter_pg33.sh

--- 10_parameter_pg33.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo The first argument is $1
-echo The second argument is $2
-echo The third argument is $3
-echo \$ $$ PID of the script
-echo \# $# count arguments
-echo \? $? last return code
-echo \* $* all the arguments
